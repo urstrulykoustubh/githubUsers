2ad86b5e0e128d6d5e0b484bb2c3c76de5d47e16
urstrulykoustubh <70585076+urstrulykoustubh@users.noreply.github.com>
2022-01-11 08:45:29 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ const Card = () => {
       <img src={avatar_url} alt={name} />
       <div>
         <h4>{name}</h4>
-        <p>@{twitter_username || 'koustubh_2912'}</p>
+        <p>@{twitter_username}</p>
       </div>
       <a href={html_url}>follow</a>
     </header>
